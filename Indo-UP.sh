2383ae1031b19eb9d0f04304b8697a0bab2df380
Wibysono <91156096+Wibysono@users.noreply.github.com>
2021-09-22 17:11:56 +0700

    Add files via upload

---
+++ Indo-UP.sh
@@ -0,0 +1,333 @@
+#!/system/xbin/bash
+
+#====================================================
+#Record lah Sepuas Kalian
+#Karna saya ga mau,, ilmu yang saya miliki
+#Berhenti di diri saya sendiri,, itu Sangat Sia Sia :)
+#Save No admin [ 089514334669 ]
+#====================================================
+
+clear
+xdg-open https://wa.wizard.id/b9c743
+
+echo "\033[31;1m██╗███╗   ██╗██████╗  ██████╗       ██╗   ██╗██████╗" 
+echo "\033[31;1m██║████╗  ██║██╔══██╗██╔═══██╗      ██║   ██║██╔══██╗"
+echo "\033[31;1m██║██╔██╗ ██║██║  ██║██║   ██║█████╗██║   ██║██████╔╝"
+echo "\033[37;1m██║██║╚██╗██║██║  ██║██║   ██║╚════╝██║   ██║██╔═══╝" 
+echo "\033[37;1m██║██║ ╚████║██████╔╝╚██████╔╝      ╚██████╔╝██║"     
+echo "\033[37;1m╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝        ╚═════╝ ╚═╝"
+echo "\033[33;1m○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○"
+echo "\033[35;1m ● \033[37;1mAuthor  \033[36;1m| \033[30;1mMr.IngsS"
+echo "\033[36;1m ● \033[37;1mRegion  \033[36;1m| \033[31;1mIndon\033[37;1mesian"
+echo "\033[32;1m ● \033[37;1mGithub  \033[36;1m| \033[30;1mhttps://github.com/IngsStools"
+echo "\033[34;1m ● \033[37;1mContact \033[36;1m| \033[30;1mSecnet@mbox.re"
+echo "\033[31;1m ● \033[37;1mProgram \033[36;1m| \033[30;1mBash/Shell"
+echo "\033[33;1m○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○ \033[37;1m[ \033[35;1mV.02 \033[37;1m]"
+echo ""
+echo "\033[36;1m ● \033[41;1m\033[37;1mINSTALLING BAHAN TERLEBIH DAHULU\033[00;1m \033[37;1m[ \033[32;1m● \033[36;1m2020 \033[37;1m]"
+echo "\033[00;1m"
+echo "\033[33;1m«●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●»
+echo "\033[36;1m╔═╦═╗        ╔══╗     ╔═╗"
+echo "\033[36;1m║║║║╠═╦═╦╦╦╗╚╗╔╩╦═╦╗║═╣"
+echo "\033[37;1m║║║║║╩╣║║║║║ ║║╬║╬║╚╬═║"
+echo "\033[37;1m╚╩═╩╩═╩╩═╩═╝ ╚╩═╩═╩═╩═╝ \033[30;1m2020"
+echo "\033[33;1m«●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●»
+echo "\033[31;1m[ \033[37;1m01 \033[31;1m] \033[33;1m● \033[32;1mBruteforce Facebook"
+echo "\033[31;1m[ \033[37;1m02 \033[31;1m] \033[33;1m● \033[32;1mAdmin-Finder"
+echo "\033[31;1m[ \033[37;1m03 \033[31;1m] \033[33;1m● \033[32;1mKnifer Tools"
+echo "\033[31;1m[ \033[37;1m04 \033[31;1m] \033[33;1m● \033[32;1mFsociety"
+echo "\033[31;1m[ \033[37;1m05 \033[31;1m] \033[33;1m● \033[32;1mSaydog-Framework"
+echo "\033[31;1m[ \033[37;1m06 \033[31;1m] \033[33;1m● \033[32;1mKeylogger Creator"
+echo "\033[31;1m[ \033[37;1m07 \033[31;1m] \033[33;1m● \033[32;1mMetasploit-Framework"
+echo "\033[31;1m[ \033[37;1m08 \033[31;1m] \033[33;1m● \033[32;1mBruteforce Instagram"
+echo "\033[31;1m[ \033[37;1m09 \033[31;1m] \033[33;1m● \033[32;1mCreate Virus Mallware"
+echo "\033[31;1m[ \033[37;1m10 \033[31;1m] \033[33;1m● \033[32;1mSQL-MAP"
+echo "\033[31;1m[ \033[37;1m11 \033[31;1m] \033[33;1m● \033[32;1mDummper Empas Moonton"
+echo "\033[31;1m[ \033[37;1m12 \033[31;1m] \033[33;1m● \033[32;1m"
+echo "\033[31;1m[ \033[37;1m13 \033[31;1m] \033[33;1m● \033[41;1m\033[37;1mInstalling Bahan\033[00;1m"
+echo "\033[31;1m[ \033[37;1m14 \033[31;1m] \033[33;1m● \033[32;1mLazymux-Tools"
+echo "\033[31;1m[ \033[37;1m15 \033[31;1m] \033[33;1m● \033[42;1m\033[37;1mUpdate Tools"
+echo "\033[31;1m[ \033[37;1m16 \033[31;1m] \033[33;1m● \033[32;1mTermux-Toolkit"
+echo "\033[31;1m[ \033[37;1m17 \033[31;1m] \033[33;1m● \033[32;1mSocialPhish"
+echo "\033[31;1m[ \033[37;1m18 \033[31;1m] \033[33;1m● \033[32;1mD-Tect Tools"
+echo "\033[31;1m[ \033[37;1m19 \033[31;1m] \033[33;1m● \033[32;1mPurebLood Tools"
+echo "\033[31;1m[ \033[37;1m20 \033[31;1m] \033[33;1m● \033[32;1mExit Program"
+
+
+echo "\033[37;1m"
+read -p "Input Number : " pilih
+if [ $pilih = "01" ]
+then
+    clear
+    echo "\033[32;1mInstalling Bruteforce Facebook \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/HostIngsS/IngsBF.git
+    cd IngsBF
+    nano pass.txt
+    python2 Bruteings.py
+fi
+
+if [ $pilih = "02" ]
+then
+    clear
+    echo "\033[32;1mInstalling Admin-Finder \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/liOJmXxsWl0
+    cd $HOME
+    git clone https://github.com/s0md3v/Breacher
+    cd Breacher
+    pip install -r paths.txt
+    echo "\033[32;1m● \033[37;1mKetik : \033[32;1mpython2 breacher.py -u \033[37;1m[ Website- Target ]"
+fi
+
+if [ $pilih = "03" ]
+then
+    clear
+    echo "\033[32;1mInstalling Knifer Tools \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/8D3f-w1MwUM
+    cd $HOME
+    xdg-open https://youtu.be/8D3f-w1MwUM
+    git clone https://github.com/BladeKnife/knf-tool
+    cd knf-tool
+    sh install.sh
+fi
+
+if [ $pilih = "04" ]
+then
+    clear
+    echo "\033[32;1mInstalling Fsociety \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/p23wxZm4WVU
+    cd $HOME
+    git clone https://github.com/Manisso/fsociety
+    cd fsociety
+    chmod +x *
+    ./fsociety.py
+fi
+
+if [ $pilih = "05" ]
+then
+    clear
+    echo "\033[32;1mInstalling Saydog-Framework \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/saydog-official/saydog-framework.git
+    cd saydog-framework
+    chmod +x *
+    pip install -r requirements.txt
+    bash setup
+fi
+
+if [ $pilih = "06" ]
+then
+    clear
+    echo "\033[32;1mInstalling Keylogger Creator \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/lJzUfWO1hQE
+    cd $HOME
+    git clone https://github.com/Zidan-ID17/Logger-X-Creator.git
+    cd Logger-X-Creator
+    python run.py
+fi
+
+if [ $pilih = "07" ]
+then
+     clear
+    echo "\033[32;1mInstalling Metasploit-Framework \033[37;1m..."
+    sleep 2
+    cd $HOME
+    curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+    ./metasploit.sh
+fi
+
+if [ $pilih = "08" ]
+then
+    clear
+    echo "\033[32;1mInstalling Bruteforce Instagram \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/M1onqb89520
+    cd $HOME
+    git clone https://github.com/evildevill/instahack
+    cd instahack
+    chmod +x *.sh
+    chmod +x setup
+    bash setup
+    bash instahack.sh
+fi
+
+if [ $pilih = "09" ]
+then
+    clear
+    echo "\033[32;1mInstalling Create Virus Mallware \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/BlackCyberAnonim/B-VIRUS.git
+    cd B-VIRUS
+    python2 B-VIRUS.py
+fi
+
+if [ $pilih = "10" ]
+then
+    clear
+    echo "\033[32;1mInstalling SQL-Map \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/njv0kk3KGgk
+    cd $HOME
+    git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+    cd sqlmap-dev
+    python2 sqlmap.py
+fi
+
+if [ $pilih = "11" ]
+then
+    clear
+    echo "\033[32;1mInstalling Dummper Empas Moonton \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/gWebMEU3wHk
+    cd $HOME
+    git clone https://github.com/limitqueenproject/empas-limit.git
+    cd empas-limit
+    bash empas
+fi
+
+if [ $pilih = "12" ]
+then
+    clear
+    echo "\033[32;1mInstalling Spam-Call Fake \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/IL4NGQW3R/spammer
+    cd spammer
+    python spam-22.py
+fi
+
+if [ $pilih = "13" ]
+then
+    clear
+    echo "\033[32;1mInstalling Installing Bahan \033[37;1m..."
+    sleep 1
+    echo "\033[32;1m● \033[37;1mMohon Tunggu \033[37;1m..."
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install python -y
+    pkg install nmap -y
+    pkg install python2 -y
+    pkg install fish
+    pkg install nano
+    pkg install wget
+    pkg install mc
+    pkg install curl
+    pkg install git
+    pkg install libxslt
+    pkg install clang
+    pkg install php
+    pip install Cython
+    pip2 install requests
+    pip2 install tqdm
+    pip install tqdm
+    pip2 install yagmail
+    pip install yagmail
+    pip install requests
+    pip install --upgrade pip
+    pip2 install mechanize
+    pip install mechanize
+    pip2 install bs4
+    pip install bs4
+    pip2 install colorama
+    pip install colorama
+    python3 -m pip install bs4
+    python3 -m pip install requests
+    echo "\033[32;1m● \033[37;1mInstalling Bahan Success"
+    sleep 10
+    echo "\033[32;1m● \033[31;1mKetikan \033[30;1m: \033[37;1msh Indo-UP.sh"
+fi
+
+if [ $pilih = "14" ]
+then
+    clear
+    echo "\033[32;1mInstalling Lazymux \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/Gameye98/Lazymux
+    cd Lazymux
+    chmod +x lazymux.py
+    python lazymux.py
+fi
+
+if [ $pilih = "15" ]
+then
+    clear
+    echo "\033[32;1mInstalling Dark-FB \033[31;1m[ \033[37;1mLog Token \033[31;1m]"
+    sleep2
+    cd $HOME
+    git clone https://github.com/IngsStools/Crack-FB-2020.git
+    cd Crack-FB-2020
+    python Dark-FB20.py
+fi
+
+if [ $pilih = "16" ]
+then
+    clear
+    echo "\033[32;1mInstalling Termux-Toolkit \033[37;1m..."
+    sleep 2
+    cd #HOME
+    git clone https://github.com/safazz/termux-toolkit
+    cd termux-toolkit
+    chmod +x *
+    bash run.sh
+fi
+
+if [ $pilih = "17" ]
+then
+    clear
+    echo "\033[32;1mInstalling SocialPhish \033[37;1m..."
+    sleep 2
+    cd #HOME
+    git clone https://github.com/xHak9x/SocialPhish
+    cd SocialPhish
+    chmod +x *
+    ./socialphish.sh
+fi
+
+if [ $pilih = "18" ]
+then
+    clear
+    echo "\033[32;1mInstalling D-TectTools \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/shawarkhanethicalhacker/D-TECT-1
+    cd D-TECT-1
+    chmod +x d-tect.py
+    python2 d-tect.py
+fi
+
+if [ $pilih = "19" ]
+then
+    clear
+    echo "\033[32;1mInstalling Pureblood-Tools"
+    sleep 2
+    cd $HOME
+    git clone https://github.com/cr4shcod3/pureblood
+    cd pureblood
+    chmod +x *
+    pip2 install -r requirements.txt
+    python2 pureblood.py
+fi
+
+if [ $pilih = "20" ]
+then
+    clear
+    echo "\033[32;1m ● \033[37;1mTerima kasih telah menggunakan tools kami"
+    echo "\033[31;1m ● \033[37;1mKami tidak bertanggung jawab atas"
+    echo "\033[37;1m.   Peretasan dengan tools ini \033[31;1m!!!"
+    sleep 3
+    echo "\033[36;1m ● \033[32;1mExiting Program \033[37;1m..."
+    exit
+    xdg-open https://wa.wizard.id/91498e
+fi
+
+    
+    
+   
